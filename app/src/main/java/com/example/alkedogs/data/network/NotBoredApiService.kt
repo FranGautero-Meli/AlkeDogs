b883eb6d7c3a3b8820ee944476ad2311c58f3e86
Francisco Gautero <francisco.gautero@mercadolibre.com>
2022-09-07 13:31:42 -0300

    Added nullables to manage different response types

--- app/src/main/java/com/example/alkedogs/data/network/NotBoredApiService.kt
+++ app/src/main/java/com/example/alkedogs/data/network/NotBoredApiService.kt
@@ -1,6 +1,6 @@
 package com.example.alkedogs.data.network
 
-import com.example.alkedogs.data.model.NotBoredActivity
+import com.example.alkedogs.data.model.NotBoredActivityResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -8,8 +8,8 @@ import retrofit2.http.Query
 
 interface NotBoredApiService {
     @GET
-    suspend fun getRandomActivity(@Query("participants") participants: Int): Response<NotBoredActivity>
+    suspend fun getRandomActivity(@Query("participants") participants: Int): Response<NotBoredActivityResponse>
 
     @GET
-    suspend fun getActivity(@Query("participants") participants: Int, @Query("type") type:String):Response<NotBoredActivity>
+    suspend fun getActivity(@Query("participants") participants: Int, @Query("type") type:String):Response<NotBoredActivityResponse>
 }
